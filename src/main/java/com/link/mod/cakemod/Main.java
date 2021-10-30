38218c7a9043304467b574c2cc8bdf6623ce3ded
Link4real <simon@die-duerrs.de>
2021-10-30 12:43:34 +0200

    birthday party 2 go

--- src/main/java/com/link/mod/cakemod/Main.java
+++ src/main/java/com/link/mod/cakemod/Main.java
@@ -1,5 +1,6 @@
 package com.link.mod.cakemod;
 
+import com.link.mod.cakemod.registry.cakes.BirthdayCakeBlock;
 import com.link.mod.cakemod.registry.cakes.ChocolateCakeBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -7,7 +8,6 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 
     public static final Block CHOCOLATE_CAKE = new ChocolateCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.2f));
-
+    public static final Block BIRTHDAY_CAKE = new BirthdayCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.2f));
 
     public static final String MOD_ID = "cakemod";
 
@@ -26,7 +26,9 @@ public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier("cakemod", "chocolate_cake"), CHOCOLATE_CAKE);
-        Registry.register(Registry.ITEM, new Identifier("cakemod", "chocolate_cake"), new BlockItem(CHOCOLATE_CAKE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "chocolate_cake"), CHOCOLATE_CAKE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chocolate_cake"), new BlockItem(CHOCOLATE_CAKE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "birthday_cake"), BIRTHDAY_CAKE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "birthday_cake"), new BlockItem(BIRTHDAY_CAKE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
     }
 }
