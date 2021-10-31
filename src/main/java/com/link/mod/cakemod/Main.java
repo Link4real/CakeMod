632b88978a9849d223805ef3ecbb4777c5c4d574
Link4real <simon@die-duerrs.de>
2021-10-31 15:31:48 +0100

    spooky update soon

--- src/main/java/com/link/mod/cakemod/Main.java
+++ src/main/java/com/link/mod/cakemod/Main.java
@@ -1,9 +1,6 @@
 package com.link.mod.cakemod;
 
-import com.link.mod.cakemod.registry.cakes.BirthdayCakeBlock;
-import com.link.mod.cakemod.registry.cakes.CarrotCakeBlock;
-import com.link.mod.cakemod.registry.cakes.ChocolateCakeBlock;
-import com.link.mod.cakemod.registry.cakes.WeddingCakeBlock;
+import com.link.mod.cakemod.registry.cakes.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -13,13 +10,16 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.lwjgl.system.CallbackI;
 
 public class Main implements ModInitializer {
 
-    public static final Block CHOCOLATE_CAKE = new ChocolateCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.2f));
-    public static final Block BIRTHDAY_CAKE = new BirthdayCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.2f));
-    public static final Block WEDDING_CAKE = new WeddingCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.2f));
-    public static final Block CARROT_BLOCK = new CarrotCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.3f));
+    public static final Block CHOCOLATE_CAKE = new ChocolateCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
+    public static final Block BIRTHDAY_CAKE = new BirthdayCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
+    public static final Block WEDDING_CAKE = new WeddingCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
+    public static final Block CARROT_BLOCK = new CarrotCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
+    public static final Block APPLE_PIE = new ApplePieBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
+    public static final Block SPOOKY_CAKE = new SpookyCakeBlock(FabricBlockSettings.of(Material.CAKE).strength(0.5f));
 
     public static final String MOD_ID = "cakemod";
 
@@ -38,5 +38,9 @@ public class Main implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "wedding_cake"), new BlockItem(WEDDING_CAKE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "carrot_cake"), CARROT_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "carrot_cake"), new BlockItem(CARROT_BLOCK, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "apple_pie"), APPLE_PIE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"apple_pie"), new BlockItem(APPLE_PIE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"spooky_cake"), SPOOKY_CAKE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "spooky_cake"), new BlockItem(SPOOKY_CAKE, new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1)));
     }
 }
