640aea8677cfbfe4cb5e3b30d782d26d6b8ea053
Link4real <simon@die-duerrs.de>
2021-11-17 13:16:48 +0100

    v1.3 update commit

--- remappedSrc/com/link/mod/cakemod/registry/cakes/ApplePieBlock.java
+++ remappedSrc/com/link/mod/cakemod/registry/cakes/ApplePieBlock.java
@@ -1,22 +1,19 @@
 package com.link.mod.cakemod.registry.cakes;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CakeBlock;
-import net.minecraft.block.Material;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class CranberryCake extends CakeBlock {
-    public CranberryCake(Settings settings) {
-        super(FabricBlockSettings.of(Material.CAKE).ticksRandomly().sounds(BlockSoundGroup.WOOL).nonOpaque().luminance(9));
+public class ApplePieBlock extends CakeBlock {
+    public ApplePieBlock(Settings settings) {
+        super(FabricBlockSettings.of(Material.CAKE).ticksRandomly().sounds(BlockSoundGroup.WOOL).nonOpaque());
     }
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.5f, 1f);
+        return VoxelShapes.cuboid(0.1f, 0f, 0.1f, 0.9f, 0.4f, 0.9f);
     }
 }
--- remappedSrc/com/link/mod/cakemod/registry/cakes/BananaCakeBlock.java
+++ remappedSrc/com/link/mod/cakemod/registry/cakes/BananaCakeBlock.java
@@ -11,12 +11,13 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class CranberryCake extends CakeBlock {
-    public CranberryCake(Settings settings) {
-        super(FabricBlockSettings.of(Material.CAKE).ticksRandomly().sounds(BlockSoundGroup.WOOL).nonOpaque().luminance(9));
+public class BananaCakeBlock extends CakeBlock {
+    public BananaCakeBlock(Settings settings) {
+        super(FabricBlockSettings.of(Material.CAKE).ticksRandomly().sounds(BlockSoundGroup.WOOL).nonOpaque());
+
     }
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.5f, 1f);
+        return VoxelShapes.cuboid(0.1f, 0f, 0.1f, 0.9f, 0.4f, 0.9f);
     }
 }
